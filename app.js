09a990cf3d76cd4c1deba7f3c121cdeb7f873dbf
KC1919 <kunal.chandra1900@gmail.com>
2021-04-24 20:15:59 +0530

    level-2 security, encrypted database

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const ejs = require("ejs");
 const mongoose=require("mongoose");
+const encrypt=require("mongoose-encryption");
 
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true, useUnifiedtopology:true});
 
@@ -14,6 +15,9 @@ const userSchema=new Schema({
   email:{type:String,required:true},
   password:{type:String,required:true}
 });
+
+const secret="Thisisourlittlesecret."
+userSchema.plugin(encrypt , {secret:secret, encryptedFields:['password']});
 
 const User=mongoose.model("User",userSchema);
 
